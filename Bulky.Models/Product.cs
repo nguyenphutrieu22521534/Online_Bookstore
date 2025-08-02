5567e405c7c8566dadc4765fb9d5e50f12bab907
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2025-08-02 23:34:34 +0700

    add fk

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -27,16 +27,18 @@ namespace Bulky.Models
         [Display(Name = "Price for 1-50")]
         [Range(1, 1000)]
         public double Price { get; set; }
-
-
         [Required]
         [Display(Name = "Price for 50+")]
         [Range(1, 1000)]
         public double Price50 { get; set; }
-
         [Required]
         [Display(Name = "Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
     }
 }
