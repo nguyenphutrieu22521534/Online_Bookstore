cd4e24570b8be182037f0e267800dd412660a56c
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2025-08-05 17:01:24 +0700

    update product custom code with datatable + sweetalerts

--- BookStore.DataAccess/Repository/IRepository/IRepository.cs
+++ BookStore.DataAccess/Repository/IRepository/IRepository.cs
@@ -10,8 +10,8 @@ namespace BookStore.DataAccess.Repository.IRepository
     public interface IRepository<T> where T : class
     {
         //T - Category
-        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
-        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false);
+        IEnumerable<T> GetAll(string? includeProperties = null);
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
         void Add(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entity);
--- BookStore.DataAccess/Repository/Repository.cs
+++ BookStore.DataAccess/Repository/Repository.cs
@@ -20,51 +20,35 @@ namespace BookStore.DataAccess.Repository
             _db = db;
             this.dbSet = _db.Set<T>();
             //_db.Products.Include(u => u.Category).Include(u => u.CategoryId);
-
+            _db.Products.Include(u => u.Category).Include(u => u.CategoryId);
         }
         public void Add(T entity)
         {
             dbSet.Add(entity);
         }
 
-        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
         {
-            IQueryable<T> query;
-            if (tracked)
-            {
-                query = dbSet;
-
-            }
-            else
-            {
-                query = dbSet.AsNoTracking();
-            }
-
+            IQueryable<T> query = dbSet;
             query = query.Where(filter);
             if (!string.IsNullOrEmpty(includeProperties))
             {
-                foreach (var includeProp in includeProperties
-                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
                 {
-                    query = query.Include(includeProp);
+                    query = query.Include(includeProperty);
                 }
             }
             return query.FirstOrDefault();
         }
 
-        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter, string? includeProperties = null)
+        public IEnumerable<T> GetAll(string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
             if (!string.IsNullOrEmpty(includeProperties))
             {
-                foreach (var includeProp in includeProperties
-                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
                 {
-                    query = query.Include(includeProp);
+                    query = query.Include(includeProperty);
                 }
             }
             return query.ToList();
